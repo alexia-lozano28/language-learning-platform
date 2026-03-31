2c19fe46714e34f9112a5a6873b35563924a84c7
alexialozp@gmail.com <alexia.lozano01@estudiant.upf.edu>
2026-03-31 16:11:40 +0200

    API KEY GLOBAL Erase

--- backend/app/core/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_API_KEY = "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"
